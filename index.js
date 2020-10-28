7dfa8bc78afc2b73eef5a060d5134857a9423b29
zll <97045924@qq.com>
2020-10-29 07:55:10 +0800

    change export file

---
+++ index.js
@@ -0,0 +1,6 @@
+import { XButton, XButtonGroup, XIcon } from './dist/index.umd'
+export {
+  XButton,
+  XButtonGroup,
+  XIcon
+}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,19 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import '../style/index.scss'
 import { XButton, XButtonGroup } from './button/src/index'
 import XIcon from './icon/src/index'
 
-const Components = {
+// const Components = {
+//   XButton,
+//   XButtonGroup,
+//   XIcon
+// }
+// Object.keys(Components).forEach(item => {
+//   Vue.component(item.name, item)
+// })
+// export default Components
+export {
   XButton,
   XButtonGroup,
   XIcon
 }
-Object.keys(Components).forEach(item => {
-  Vue.component(item.name, item)
-})
-export default Components
